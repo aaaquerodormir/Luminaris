fd4996add619657022fe0b07c51bece3413f220c
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2025-08-21 16:54:21 -0300

    Prefabs Menu + PauseMenu

--- Luminaris/Assets/Scripts/Game/GameOverMenu.cs
+++ Luminaris/Assets/Scripts/Game/GameOverMenu.cs
@@ -5,6 +5,9 @@ public class GameOverMenu : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
     [SerializeField] private string mainMenuScene = "MainMenu";
+    [SerializeField] private GameObject confirmacaoPanel;
+
+    private System.Action acaoConfirmada;
 
     public void TentarNovamente()
     {
@@ -13,16 +16,42 @@ public class GameOverMenu : MonoBehaviour
 
     public void VoltarMenuPrincipal()
     {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(mainMenuScene);
+        // Mostra o painel de confirmação antes de sair
+        AbrirConfirmacao(() =>
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(mainMenuScene);
+        });
     }
 
     public void SairDoJogo()
     {
+        // Mostra o painel de confirmação antes de sair
+        AbrirConfirmacao(() =>
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #else
-        Application.Quit();
+            Application.Quit();
 #endif
+        });
+    }
+
+    private void AbrirConfirmacao(System.Action acao)
+    {
+        confirmacaoPanel.SetActive(true);
+        acaoConfirmada = acao;
+    }
+
+    public void Confirmar()
+    {
+        confirmacaoPanel.SetActive(false);
+        acaoConfirmada?.Invoke();
+    }
+
+    public void Cancelar()
+    {
+        confirmacaoPanel.SetActive(false);
+        acaoConfirmada = null;
     }
 }
