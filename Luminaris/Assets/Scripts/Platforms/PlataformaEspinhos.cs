d0cc3d07de0e9c2f7bbae513c3a362d998e3d449
TUTUJOOJ <spyrojorge@gmail.com>
2025-08-31 11:40:56 -0300

    LevelDesign + Animation Plataforma Instavel + Better Animation Espinhos
    V5

--- Luminaris/Assets/Scripts/Platforms/PlataformaEspinhos.cs
+++ Luminaris/Assets/Scripts/Platforms/PlataformaEspinhos.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlataformaEspinhos : MonoBehaviour
 {
-    public float bounceForce = 10f;
     public int damage = 1;
+    public float animationInterval = 2f; // pausa
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(PlayAnimationWithPauseLoop());
+    }
+
+    private IEnumerator PlayAnimationWithPauseLoop()
+    {
+        while (true)
+        {
+            animator.Play("Espinhos", 0, 0f); // Toca a animacao do inicio
+            yield return new WaitForSeconds(GetAnimationClipLength("Espinhos"));
+            yield return new WaitForSeconds(animationInterval); // Espera entre as anima��es
+        }
+    }
+
+    private float GetAnimationClipLength(string clipName)
+    {
+        foreach (var clip in animator.runtimeAnimatorController.animationClips)
+        {
+            if (clip.name == clipName)
+            {
+                return clip.length;
+            }
+        }
+
+        Debug.LogWarning("Animation clip not found: " + clipName);
+        return 1f;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -11,6 +43,7 @@ public class PlataformaEspinhos : MonoBehaviour
         {
             HandlePlayerBounce(collision.gameObject);
         }
+
         var respawn = collision.GetComponent<PlayerRespawn>();
         if (respawn != null)
         {
@@ -21,12 +54,9 @@ public class PlataformaEspinhos : MonoBehaviour
     private void HandlePlayerBounce(GameObject player)
     {
         Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
-
         if (rb)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-
-            rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
         }
     }
 }
