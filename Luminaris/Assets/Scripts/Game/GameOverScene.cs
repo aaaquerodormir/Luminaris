df6cec870cf74b32a6b94f888479a6dba798272b
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2025-11-11 11:45:08 -0300

    Game Over Scene
    v1

---
+++ Luminaris/Assets/Scripts/Game/GameOverScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameOverScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
